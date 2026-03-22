5a000f4daf8587ab84ecf657c935f16263acde0f
Edgar Jung <edgar.jung@elropro.de>
2026-03-22 08:09:38 +0100

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMwODQ4NQ/spectral.js?branch_slug=main&token=6c6ffbcc-6fc0-45c5-8de2-1c3664d281c6";
+export default { extends: ruleset };
